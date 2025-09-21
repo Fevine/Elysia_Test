c6e64e538fbf5dbd05e3f0d73668a4146e0f3fce
Albinoni <albinoni1423@gmail.com>
2025-09-21 07:07:33 +0400

    user route connected to project

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import Elysia from "elysia"
 import { openapi } from "@elysiajs/openapi"
 import { swagger } from "@elysiajs/swagger"
 import { cors } from "@elysiajs/cors"
+import { userRoutes } from "./routes/user.route"
 
 export const app = new Elysia()
   // Apply the openapi plugin
+  .use(userRoutes)
   .use(openapi({}))
   .use(cors())
   .use(swagger())
